c91e9b9473783ee612d8bda3c2fa4894c7cc93d4
Mai Anh Tu <vviptma96@gmail.com>
2021-04-29 08:21:45 +0700

    Connect Firebase and Submit

---
+++ main.js
@@ -0,0 +1,64 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAZeQ59fwocpnM-ClmGi5CrrdcKrUrqlmY",
+    authDomain: "fir-contact-beee6.firebaseapp.com",
+    projectId: "fir-contact-beee6",
+    storageBucket: "fir-contact-beee6.appspot.com",
+    messagingSenderId: "757677973130",
+    appId: "1:757677973130:web:64411482ad40b3de515773",
+    measurementId: "G-JEWB4XP7FY"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var company = getInputVal('company');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company:company,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
